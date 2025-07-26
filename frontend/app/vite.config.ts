c3b60365474834c5d36ec9ddcec179f7ce2e01e5
AdityaVyas <58148990+Adity7@users.noreply.github.com>
2025-07-26 16:30:48 +0530

    Update vite.config.ts

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Use relative paths for assets
   plugins: [react()],
   resolve: {
     alias: {
@@ -13,10 +14,23 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
+    emptyOutDir: true,
     chunkSizeWarningLimit: 1000, // Set chunk size warning limit to 1000 kB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          vendor: ['@radix-ui/react-dialog', '@radix-ui/react-dropdown-menu'],
+        },
+      },
+    },
   },
   server: {
     port: 8080,
     host: true,
   },
+  preview: {
+    port: 8080,
+    host: true,
+  }
 })
